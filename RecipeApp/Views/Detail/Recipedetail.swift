67ceee4fc9602100db184a2f39594456cfe70781
koto729 <ko.wingto.11@gmail.com>
2023-01-13 01:19:39 +0800

    finish the recipe detail

--- RecipeApp/Views/Detail/Recipedetail.swift
+++ RecipeApp/Views/Detail/Recipedetail.swift
@@ -10,8 +10,30 @@ import SwiftUI
 struct Recipedetail: View {
     let contact: RecipeAPI
     var body: some View {
-        Text(contact.title)
-    }
+        ScrollView{
+            VStack(spacing: 30){
+                Text(contact.title)
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.center)
+                
+                    
+                    VStack(alignment: .leading, spacing: 20){
+                        Text("Inredients")
+                            .font(.headline)
+                        Text(contact.ingredients)
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 20){
+                        Text("Direcions")
+                            .font(.headline)
+                        
+                        Text(contact.instructions)
+                    }
+                }
+            }
+        }
+    
 }
 
 
